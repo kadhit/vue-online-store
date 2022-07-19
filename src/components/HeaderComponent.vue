e751c7c18c66b5e75f47bab55fa3140aea6350ff
kadhit <92635388+kadhit@users.noreply.github.com>
2022-07-19 22:30:51 +0700

    added HeaderComponent responsiveness

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,16 +11,42 @@
         <div class="text-5xl font-serif">
           <h1 class="font-light">hi<span class="font-bold">there.</span></h1>
         </div>
-        <!-- Menu Items -->
-        <div class="hidden md:flex space-x-16 font-sans font-normal">
-          <a href="">About</a>
-          <a href="">Our Work</a>
-          <a href="">Testimonials</a>
-          <a href="">Contact</a>
+        <div class="flex items-center justify-end space-x-12">
+          <!-- Menu Items -->
+          <div class="hidden md:flex space-x-12 font-sans font-normal">
+            <a href="">About</a>
+            <a href="">Our Products</a>
+            <a href="">Contact</a>
+          </div>
+          <!-- Shop now Button -->
+          <a
+            href=""
+            class="hidden md:inline py-3 px-6 bg-emerald-400 text-white font-semibold rounded-full"
+            >Shop now</a
+          >
         </div>
-        <button class="block md:hidden">
-          <HamburgerIcon />
+        <button
+          @click="isToggled = !isToggled"
+          class="block relative md:hidden"
+        >
+          <div v-if="isToggled === false"><HamburgerIcon /></div>
+          <div v-else><CloseIcon /></div>
         </button>
+      </div>
+    </div>
+    <div
+      v-if="isToggled === true"
+      class="block absolute index-10 w-screen h-screen bg-slate-800 top-20 right-0 md:hidden"
+    >
+      <div class="flex flex-col pt-8 space-y-8 justify-end items-center">
+        <a href="">About</a>
+        <a href="">Our Products</a>
+        <a href="">Contact</a>
+        <a
+          href=""
+          class="py-3 px-6 bg-emerald-400 text-white font-semibold rounded-full"
+          >Shop now</a
+        >
       </div>
     </div>
   </nav>
@@ -28,11 +54,18 @@
 
 <script>
 import HamburgerIcon from "./icons/HamburgerIcon.vue";
+import CloseIcon from "./icons/CloseIcon.vue";
 
 export default {
   name: "HeaderComponent",
   components: {
     HamburgerIcon,
+    CloseIcon,
+  },
+  data() {
+    return {
+      isToggled: false,
+    };
   },
 };
 </script>
--- src/components/icons/CloseIcon.vue
+++ src/components/icons/CloseIcon.vue
@@ -11,12 +11,12 @@
     <path
       stroke-linecap="round"
       stroke-linejoin="round"
-      d="M4 6h16M4 12h16M4 18h16"
+      d="M6 18L18 6M6 6l12 12"
     />
   </svg>
 </template>
 <script>
 export default {
-  name: "HamburgerIcon",
+  name: "CloseIcon",
 };
 </script>
